326ecc6a3461e8f60866889539561a4202742e45
RyanLin5967 <idident5967@gmail.com>
2026-04-12 21:41:15 -0400

    fixed bug with the options on individual recipe page not working

--- frontend/src/components/IngredientsOption.tsx
+++ frontend/src/components/IngredientsOption.tsx
@@ -3,15 +3,13 @@ import {Dot} from "lucide-react-native"
 
 export default function IngredientsOption({ingredients}: {ingredients: string[]}){
   return (
-    <View>
+    <ScrollView>
       {ingredients?.map((ingr, index) => (
-        <ScrollView key={index}>
-          <View key={index} className="flex flex-row px-3 py-2 bg-[rgb(59,61,69)] m-2 mx-4 rounded-3xl">
-            <View className=""><Dot color={"[rgb(237,84,19)]"} size={40}></Dot></View>
-            <Text className="mt-2 text-white text-xl font-bold flex-1 flex-shrink pb-2">{ingr}</Text>
-          </View>
-        </ScrollView>
+        <View key={index} className="flex flex-row px-3 py-2 bg-[rgb(59,61,69)] m-2 mx-4 rounded-3xl">
+          <View className=""><Dot color={"[rgb(237,84,19)]"} size={40}></Dot></View>
+          <Text className="mt-2 text-white text-xl font-bold flex-1 flex-shrink pb-2">{ingr}</Text>
+        </View>
       ))}
-    </View>
+    </ScrollView>
   )
 }
