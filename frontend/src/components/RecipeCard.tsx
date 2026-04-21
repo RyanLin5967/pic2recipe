79afde87fbce48ac65f5bc15e289733d09fa9412
RyanLin5967 <idident5967@gmail.com>
2026-04-20 23:54:40 -0400

    added minor ui improvements 2

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -14,7 +14,7 @@ export default function RecipeCard({recipe, ingredients}: {recipe: Recipe, ingre
       <Text className="text-white font-bold text-2xl pl-5 pt-4">{recipe.title}</Text>
       <View className="flex flex-row">
         <View className="ml-5 mt-2 p-1 px-2 bg-[rgb(237,84,19)] rounded-2xl"><Text className="font-bold">{Math.round((recipe.similarity)*100)}% Match</Text></View>
-        <View className="ml-2 mt-2 p-1 px-2 bg-[rgb(37,72,66)] rounded-2xl"><Text className="text-[rgb(43,201,154)] font-bold">{recipe.difficulty ?? "Unknown"}</Text></View>
+        <View className={recipe.difficulty === "Easy"? "ml-2 mt-2 p-1 px-2 bg-[rgb(37,72,66)] rounded-2xl": recipe.difficulty === "Medium"? "ml-2 mt-2 p-1 px-2 bg-[rgb(72,58,30)] rounded-2xl": "ml-2 mt-2 p-1 px-2 bg-[rgb(72,37,37)] rounded-2xl"}><Text className={recipe.difficulty === "Easy"? "text-[rgb(43,201,154)] font-bold": recipe.difficulty === "Medium"? "text-[rgb(223,133,31)] font-bold": "text-[rgb(212,50,50)] font-bold"}>{recipe.difficulty ?? "Unknown"}</Text></View>
       </View>
       <Text className="ml-5 my-4 text-[rgb(167,167,167)]">{usesWhatIngredients()}</Text>
       <View className="flex flex-row">
