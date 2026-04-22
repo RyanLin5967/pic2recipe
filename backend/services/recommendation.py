0cc1c7a5d3233a49f1b6b4e3339ce695927a400f
RyanLin5967 <idident5967@gmail.com>
2026-04-22 16:33:50 -0400

    fixed typo

--- backend/services/recommendation.py
+++ backend/services/recommendation.py
@@ -3,6 +3,6 @@
 from repositories.recipe_repo import get_recipe
 
 def get_recommendations(ingredients, model, db):
-    query = " ,".join(ingredients)
+    query = ", ".join(ingredients)
     embedding = model.encode(query).tolist()
     return get_recipe(db, embedding)
