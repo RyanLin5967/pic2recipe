d6d825858f213ce60700bea00299384f742ee3dd
RyanLin5967 <idident5967@gmail.com>
2026-04-28 22:14:17 -0400

    fixed typo, cook time being bad type

--- frontend/src/database/models/Favorite.ts
+++ frontend/src/database/models/Favorite.ts
@@ -2,14 +2,14 @@ import { Model } from '@nozbe/watermelondb'
 import { field, date, readonly } from '@nozbe/watermelondb/decorators'
 
 export default class Favorite extends Model {
-    static table= "ingredients"
+    static table= "favorites"
 
     @field("recipe_id") recipeId!: number
     @field("title") title!: string
     @field("ingredients_json") ingredientsJson!: string
     @field("directions_json") directionsJson!: string
     @field("similarity") similarity!: number
-    @field("cook_time_minutes") cookTimeMinutes!: number | null
+    @field("cook_time_minutes") cookTimeMinutes!: any
     @field("difficulty") difficulty!: string | null
     @field("equipment_json") equipmentJson!: string | null
     @readonly @date("create_at") createdAt!: Date
